b2ddd613f4fbb286f9d9baacd094387eb8a5a600
Midas <richardisraelmagalhaes@gmail.com>
2023-07-11 13:37:54 -0300

    Responsividade page-about

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import svgBg from './components/svg/svgBg.vue'
+import svgCode from './components/vue-design/svgCode.vue';
 //  import svgAbout from './components/vue-design/svgAbout.vue'
 import svgHome from './components/vue-design/svgHome.vue'
+import svgPc from './components/vue-design/svgPc.vue';
 //  import svgPc from './components/vue-design/svgPc.vue'
 //  import svgProjects from './vue-design/components/svgProjects.vue'
 </script>
@@ -41,11 +43,29 @@ import svgHome from './components/vue-design/svgHome.vue'
       </div>
     </div>
   </div>
+  <div class="about-container">
+    <div class="about-center">
+      <svgCode class="svg-code" />
+      <div class="about-line"></div>
+      <h1>Sobre mim</h1>
+      <div class="about-left">
+        <svgPc class="svg-pc" />
+      </div>
+      <div class="about-right">
+        <div class="about-column">
+          <h2>Oi, <br> Me chamo <span>Richard</span>.</h2>
+          <p>Tenho 20 anos e atualmente moro no Rio de Janeiro. Sempre gostei de games e tecnologia e foi através dos
+            jogos que tive interesse na área de programação. Hoje sigo minha carreira dominando as tecnologias ReactJs e
+            VueJs. Estudo um pouco de PHP para ter conhecimentos sólidos em back-end pra um dia me tornar um Dev
+            FullStack. ☕</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 // VAR
-
 $color-primary: #212428;
 $color-secondary: #2B3037;
 $main-vue: #00FF7F;
@@ -260,6 +280,11 @@ $main-vue: #00FF7F;
 }
 
 @media screen and (max-width: 1110px) {
+
+  .home-container {
+    margin-bottom: 150px !important;
+  }
+
   .home-content {
     display: flex;
     align-items: center;
@@ -300,7 +325,8 @@ $main-vue: #00FF7F;
       font-size: .7rem !important;
     }
 
-    .btn-left,.btn-right {
+    .btn-left,
+    .btn-right {
       bottom: 30px !important;
       width: 150px !important;
     }
@@ -317,6 +343,189 @@ $main-vue: #00FF7F;
 
 }
 
-
 //===== HOME END =====
+
+.about-container {
+
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+
+  .about-center {
+    width: 1500px;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    position: relative;
+
+    .svg-code {
+      width: 2000px;
+      position: absolute;
+      right: -650px;
+      display: flex;
+    }
+
+    h1 {
+      position: absolute;
+      font-family: Open sans;
+      color: #ffff;
+      font-size: 3rem;
+      font-weight: 500;
+      right: 300px;
+      top: 160px;
+    }
+
+    .about-line {
+      position: absolute;
+      width: 600px;
+      height: 5px;
+      border-radius: 5px 0 0 5px;
+      background-color: $main-vue;
+      right: 600px;
+      top: 195px;
+    }
+
+    .about-left {
+      width: 50%;
+      height: 100%;
+      position: relative;
+
+      .svg-pc {
+        width: 1000px;
+        position: absolute;
+        right: -100px;
+      }
+
+    }
+
+    .about-right {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: start;
+
+      h2 {
+        color: #ffff;
+        font-size: 3rem;
+        font-family: Open sans;
+        font-weight: 400;
+      }
+
+      span {
+        color: $main-vue;
+      }
+
+      p {
+        color: #ffff;
+        font-size: 1rem;
+        font-family: Open sans;
+        font-weight: 400;
+        width: 500px;
+      }
+
+      .about-row {
+        display: flex;
+        flex-direction: column;
+      }
+
+    }
+
+  }
+}
+
+@media screen and (max-width: 1110px) {
+
+
+
+  .about-center {
+    padding-top: 350px !important;
+    display: flex !important;
+    flex-direction: column !important;
+    align-items: center !important;
+    justify-content: center !important;
+    height: 1500px !important;
+
+    .svg-pc {
+      width: 800px !important;
+      left: 40% !important;
+      transform: translateX(-50%) !important;
+    }
+
+
+    .about-left {
+      width: 100% !important;
+      display: flex !important;
+      justify-content: center !important;
+    }
+
+    .about-right {
+      width: 100% !important;
+      display: flex !important;
+      justify-content: center !important;
+    }
+
+    h2 {
+      text-align: center;
+    }
+
+    h1 {
+      top: 520px !important;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 300px;
+      display: flex;
+      justify-content: center;
+    }
+  }
+
+  .about-line {
+    top: 630px !important;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .svg-code {
+    display: none !important;
+  }
+}
+
+@media screen and (max-width: 600px) {
+
+  .about-center {
+    h1 {
+      top: 320px !important;
+      font-size: 2rem !important;
+    }
+
+    .about-line {
+      top: 430px !important;
+      width: 400px !important;
+    }
+
+    .svg-pc {
+      width: 600px !important;
+      bottom: -100px !important;
+    }
+  }
+
+  .about-right {
+    align-items: start !important;
+
+    h2 {
+      font-size: 2rem !important;
+    }
+
+    p {
+      font-size: .8rem !important;
+      width: 350px !important;
+    }
+  }
+
+}
+
+//===== ABOUT END =====
 </style>
