b7124fe8540d16e453d57b9f46de9476223ea376
Midas <richardisraelmagalhaes@gmail.com>
2023-07-20 13:34:29 -0300

    Testando config para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: { additionalData: `@import "src/scss/app.scss";` }
-  //   }
-  // }
+  base: '/deploy-testing',
+  plugins: [vue()] 
 })
